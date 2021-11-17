0e22ab91f2dfdec5bffc87e86f9c798af8316ee4
AudreyCourtiol <audrey.courtiol@edu.ece.fr>
2021-11-17 12:22:36 +0100

    board commented

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -8,23 +8,23 @@ import java.util.Random;
 
 
 public class Board {
+    //Attributes of the board
     Player player= new Player();
     final int SIZE = 12;
     String[][] m_2DBoard = new String[SIZE][SIZE];
-    Boolean[][] isFieldEmpty = new Boolean[SIZE][SIZE];
 
     //We randomize the number of initial objects
     Random R_nbOfEnemies = new Random();
     Random R_nbOfObstacles = new Random();
-    int nbOfEnemies = R_nbOfEnemies.nextInt((SIZE - 2) + 1) + 2; // we get maximum half of the board filled with animals and min. 2
+    int nbOfEnemies = R_nbOfEnemies.nextInt((SIZE - 2) + 1) + 2; // we get maximum half of the board filled with enemies and min. 2
     int nbOfObstacles = R_nbOfObstacles.nextInt(SIZE); //we get maximum half of the board filled with obstacles
 
-    ArrayList<Enemy> m_enemies = new ArrayList<>(nbOfEnemies); //we keep track of all our animals
-    ArrayList<Obstacle> m_obstacles = new ArrayList<>(nbOfObstacles);
+    ArrayList<Enemy> m_enemies = new ArrayList<>(nbOfEnemies); //we keep track of all our enemies
+    ArrayList<Obstacle> m_obstacles = new ArrayList<>(nbOfObstacles); //we keep track of all our obstacles
 
 
     public Board(){
-        //We create a board with 10x10 fields
+        //We create a board with 10x10 usable fields
         for (int i = 0; i < this.SIZE; i++){
             for(int j = 0; j < this.SIZE; j++){
                 //If it's a border, we put a star
@@ -33,12 +33,6 @@ public class Board {
                 } else { //else we just put a space
                     m_2DBoard[i][j] = " ";
                 }
-            }
-        }
-        //We set every field as empty at the creation of the board
-        for(int j = 0; j < this.SIZE-1; j++){
-            for(int k = 0; k < this.SIZE-1; k++){
-                this.isFieldEmpty[j][k] = false;
             }
         }
     }
@@ -53,16 +47,21 @@ public class Board {
         }
     }
 
-    //We put randoms numbers of obstacles and enemies on the board
+    //In this method we put randoms numbers of obstacles and enemies on the board
     void fillBoardAtTheStart(){
+        //Random start position for the player
         Random R_start_x = new Random();
         Random R_start_y = new Random();
-        //we put the enemy on the board
+
         int x1 = R_start_x.nextInt(SIZE - 2) + 1;
         int y1 = R_start_y.nextInt(SIZE - 2) + 1;
-        this.m_2DBoard[x1][y1] = this.player.getSign();
-        this.player.setPosition(new Point(x1,y1));
-        for(int i = 0; i < this.nbOfEnemies; i++){
+
+        this.m_2DBoard[x1][y1] = this.player.getSign(); //we place the player on the board
+        this.player.setPosition(new Point(x1,y1)); //we set this position as the player's position
+
+
+        //Random start position for the enemies
+        for(int i = 0; i < this.nbOfEnemies; i++){ //for as many enemies as we have
             Enemy a = new Enemy();
             a.player = this.player; //we connect the enemy player to the game player
 
@@ -79,9 +78,11 @@ public class Board {
             //we keep track of this enemy's position
             a.setPosition(new Point(x,y));
 
+            //We add the enemy to our list so we don't lose it
             this.m_enemies.add(a);
         }
 
+        //Random position for the obstacles
         for(int i = 0; i < this.nbOfObstacles; i++){
             Obstacle o = new Obstacle();
 
@@ -95,11 +96,13 @@ public class Board {
             int y = R_column2.nextInt(SIZE - 2) + 1;
             this.m_2DBoard[x][y] = o.getSign();
 
+            //We keep track of the obstacle
             o.setPosition(new Point(x,y));
             this.m_obstacles.add(o);
         }
     }
 
+    //This method moves the enemies towards the player
     void moveEnemies(){
         //we go through the board
         for(int i = 0; i < this.SIZE; i++){
@@ -110,6 +113,7 @@ public class Board {
             }
         }
 
+        //Here we will put the enemies that died and should be taken off the board
         ArrayList<Enemy> toDelete = new ArrayList<>();
 
         //We make all enemies move in the player's direction
@@ -128,43 +132,47 @@ public class Board {
                     this.m_2DBoard[e.getPosition().x][e.getPosition().y]= e.getSign(); //we draw it on the board
                 }
             }
-            else //if new pos is a border
+            else //if the new position is a border
             {
                 this.m_2DBoard[oldPos.x][oldPos.y]= e.getSign(); //we redraw the enemy where it was before, and it doesn't move
             }
         }
 
-        //If an enemy has been added to toDelete, we remove it from the list of enemies using an iterator
+        //If an enemy has been added to toDelete, we remove it from the list of enemies
         this.m_enemies.removeIf(toDelete::contains);
     }
 
-    Integer checkNumberOfEnemies(){ //The game ends if there are no more enemies
+    //This method checks how many enemies are left on the board and returns that number
+    Integer checkNumberOfEnemies(){
         int EnemiesLeft = 0;
         //we go through our board
         for (String[] strings : m_2DBoard) {
             for (int j = 0; j < m_2DBoard.length; j++) {
                 if (Objects.equals(strings[j], "X")) { //if there is an enemy
-                    EnemiesLeft++; //we increase our number of animals left
+                    EnemiesLeft++; //we increase our number of enemies left
                 }
             }
         }
         return EnemiesLeft;
     }
 
-    void MovesP(){ //to move the player on the board
+    //This method moves the player on the board
+    void MovesP(){
         Point oldPlayer=new Point(this.player.getPosition().x,this.player.getPosition().y); //we store the current position of the player
         this.m_2DBoard[this.player.getPosition().x][this.player.getPosition().y]= " "; //we set that position as empty on the board
         int test=1;
+
+        //We use a try/catch to catch an error if there is one
         try {
             while (test!=0)
             {
-                this.player.movePlayer(); //move the player
+                this.player.movePlayer(); //move the player according to what the user asked
                 //If the position is NOT on a border, we show the player on the board at its new position
                 if (((this.player.getPosition().x >0) && (this.player.getPosition().x <11)) && ((this.player.getPosition().y>0) && (this.player.getPosition().y<11))) {
                     this.m_2DBoard[this.player.getPosition().x][this.player.getPosition().y]= this.player.sign;
                     test=0;
                 }
-                else //if new pos is a border
+                else //if the new position is a border
                 {
                     System.out.println("Out of game board try again");
                     this.m_2DBoard[this.player.getPosition().x][this.player.getPosition().y]= "*";
@@ -174,9 +182,10 @@ public class Board {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
+    //This method runs the game
+    //It is called in a try/catch to catch an error in case there is one
     void playGame() throws IOException {
         int EnemiesLeft;
         boolean play=true; //this tells us if the game is going on. When it stops being true, the game is over
