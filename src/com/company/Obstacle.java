9e79f47f47b3da252869f828960c102b7a0941fe
AudreyCourtiol <audrey.courtiol@edu.ece.fr>
2021-11-17 11:55:22 +0100

    obstacle commented

--- src/com/company/Obstacle.java
+++ src/com/company/Obstacle.java
@@ -3,16 +3,20 @@ package com.company;
 import java.awt.*;
 
 public class Obstacle {
+    //Attributes of an obstacle
     String sign;
     Point position = new Point();
 
+    //Creation of an obstacle
     public Obstacle(){
         this.sign = "#";
     }
 
+    //To get the obstacle's information
     String getSign(){return this.sign; }
     Point getPosition(){return this.position; }
 
+    //To set the obstacle's position
     void setPosition(Point a){
         this.position.setLocation(a);
     }
