ab01bb9a641ee533ab2bab18feadcc42ea04e760
AudreyCourtiol <audrey.courtiol@edu.ece.fr>
2021-11-17 11:53:47 +0100

    main commented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,18 @@
 package com.company;
 
+//To allow us to easily catch errors
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
         Board board = new Board();
-        //System.out.println("The game has been initialized.");
+
+        //Initialization
         board.fillBoardAtTheStart();
         board.displayBoard();
+
+        //Running the game
         try {
             board.playGame();
         } catch (IOException e) {
